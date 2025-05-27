ae0f04d4c5424a858218da089b0902a61a36645c
DiegoSantiagos <diegoisantiagos@gmail.com>
2025-05-27 17:09:50 -0400

    hola

--- gestion/src/main/java/com/ferramas/gestion/controller/CiudadController.java
+++ gestion/src/main/java/com/ferramas/gestion/controller/CiudadController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 
 @RestController
-@RequestMapping("/api/ciudades")
+@RequestMapping("/api/ciudades/")
 public class CiudadController {
 
     @Autowired
--- gestion/src/main/java/com/ferramas/gestion/controller/DespachoController.java
+++ gestion/src/main/java/com/ferramas/gestion/controller/DespachoController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 
 @RestController
-@RequestMapping("/api/despachos")
+@RequestMapping("/api/despachos/")
 public class DespachoController {
 
     @Autowired
