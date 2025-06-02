f801a2303d481613f545ef19066bfbddf0e82493
Natalialeiva20 <mentitamaracuya@gmail.com>
2025-06-02 01:49:39 -0400

    xd no funcionó lo que puse en admin pq me dió error en mi catálogo
    :( pipipi, lo intento mañana

--- ferraMasDjango/tienda/urls.py
+++ ferraMasDjango/tienda/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from . import views
+from .import views
 
 urlpatterns = [
+    path('empleados/', views.ver_empleados, name='ver_empleados'),
+    path('comunas/', views.ver_comunas, name='ver_comunas'),
     path('catalogo/', views.verCatalogo, name='ver_catalogo'),
     path('productos/', views.verProductos, name='ver_productos'),
     path('producto/<str:producto_id>/', views.verProductoDetalle, name='ver_producto'),
     path('', views.ver_home, name='home'),
-    path('sedes/', views.ver_sedes, name='ver_sedes'),
-    path('administracion/', views.administracion, name='administracion'),
+    path('sedes/', views.ver_sedes, name='ver_sedes'),  
+    path('seleccionar_sede/', views.seleccionar_sede, name='seleccionar_sede'),  
+    path('cambiar-sede/', views.cambiar_sede_ajax, name='cambiar_sede_ajax')
 ]
+
